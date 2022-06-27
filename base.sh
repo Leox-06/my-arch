2aa4944d7cc7f14930c17716a93fa1c40a54c823
Leonard <leoxmimmo@gmail.com>
2022-06-27 10:41:01 +0000

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pacman -S --noconfirm git vim neofetch grub ntfs-3g networkmanager
+
+grub-install --target=i386-pc /dev/sdb
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
+/bin/echo -e "\e[1;32mDone!\e[0m"
