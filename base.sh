a2460bc13e0b554e93cb450aeb6fb767152e6538
Leonard <leoxmimmo@gmail.com>
2022-06-28 15:13:56 +0200

    Update base.sh
    enable os-prober

--- base.sh
+++ base.sh
@@ -2,6 +2,9 @@
 
 pacman -S --noconfirm git vim neofetch sudo grub ntfs-3g os-prober networkmanager
 
+# Enable os-prober
+echo "GRUB_DISABLE_OS_PROBER=\"false\"" >> /etc/default/grub
+
 grub-install --target=i386-pc /dev/sdb
 grub-mkconfig -o /boot/grub/grub.cfg
 
